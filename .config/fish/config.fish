475dda4a4204fe40fd66c0170cf47cedeb189dc9
anshumansingh <anshumansingh0010>
2025-12-06 23:19:41 +0530

    something

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,7 +9,8 @@ if status is-interactive
     # Better ls
     alias ls='eza --icons --group-directories-first -1'
     alias gitbackup="~/.scripts/gitpush.sh"
-    alias rm="trash-put"	
+    alias rm="trash-put"
+    alias lively="~/.scripts/mpvpaper/set_video_wallpaper.sh"	
     # Abbrs
     abbr gd 'git diff'
     abbr ga 'git add .'
